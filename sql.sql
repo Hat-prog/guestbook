bb748d1428de7f4e17bc6a733c3bf552e8ec4712
Hat <thokenm3@gmail.com>
2026-02-01 15:32:04 +0000

    Update messages table with new insert statements
    Added multiple insert statements for messages.

--- sql.sql
+++ sql.sql
@@ -1,8 +1,11 @@
--- copy this into SUPABASE!!!
 CREATE TABLE messages (
     id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     msg_name TEXT,
     content TEXT
-)
+);
 
-INSERT INTO messages (msg_name, content) VALUES ('hello there','general kenobi')
+INSERT INTO messages (msg_name, content) VALUES 
+('Andrey', 'Just passing through and leaving my mark here'),
+('Dimitrov', 'Had a great time checking this out, would definitely recommend it.'),
+('Ashley', 'Great vibes here, I''ll definitely be back'),
+('Lily', 'Just passing through and leaving my mark here');
